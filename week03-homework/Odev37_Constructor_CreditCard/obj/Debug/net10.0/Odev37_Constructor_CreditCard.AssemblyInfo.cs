d1a8649cfe5989cceaa57f545910cdd32006245a
İsmail <ismail.ipekci18@gmail.com>
2026-08-08 09:30:22 +0300

    4. hafta

--- week03-homework/Odev37_Constructor_CreditCard/obj/Debug/net10.0/Odev37_Constructor_CreditCard.AssemblyInfo.cs
+++ week03-homework/Odev37_Constructor_CreditCard/obj/Debug/net10.0/Odev37_Constructor_CreditCard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odev37_Constructor_CreditCard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddebb38f41dff9a3937b7498768eb2bd5fae2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odev37_Constructor_CreditCard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odev37_Constructor_CreditCard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- week03-homework/Odev39_Constructor_FlightTicket/obj/Debug/net10.0/Odev39_Constructor_FlightTicket.AssemblyInfo.cs
+++ week03-homework/Odev39_Constructor_FlightTicket/obj/Debug/net10.0/Odev39_Constructor_FlightTicket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odev39_Constructor_FlightTicket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddebb38f41dff9a3937b7498768eb2bd5fae2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odev39_Constructor_FlightTicket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odev39_Constructor_FlightTicket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
